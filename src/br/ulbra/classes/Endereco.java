3d7c31520bd801e0bf6f3c86cdd96050a7108cbd
Jeferson Leon <jefersonleonblue@gmail.com>
2021-10-04 19:40:45 -0300

    Projeto API ViaCep - Aula: 04/10

---
+++ src/br/ulbra/classes/Endereco.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.classes;
+
+/**
+ *
+ * @author Jeferson
+ */
+public class Endereco {
+    String logradouro;
+    String bairro;
+    String localidade;
+    String uf;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+      
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "logradouro=" + logradouro + ", bairro=" + bairro + ", localidade=" + localidade + ", uf=" + uf + '}';
+    }
+    
+    
+}
---
+++ src/br/ulbra/classes/Util.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.classes;
+
+/**
+ *
+ * @author Jeferson
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Util {
+    public static String converteJsonEmString(BufferedReader buffereReader) throws IOException {
+        String resposta, jsonEmString = "";
+        while ((resposta = buffereReader.readLine()) != null) {
+            jsonEmString += resposta;
+        }
+        return jsonEmString;
+    }
+}
